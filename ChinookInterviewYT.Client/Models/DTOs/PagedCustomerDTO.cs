3fbf69f0100a92bfa312003061b8bd26f489181f
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-08 00:40:43 -0600

    Modified PagedCustomerDTO to enable the search results display.

--- ChinookInterviewYT.Client/Models/DTOs/PagedCustomerDTO.cs
+++ ChinookInterviewYT.Client/Models/DTOs/PagedCustomerDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace ChinookInterviewYT.Client.Models.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace ChinookInterviewYT.Client.Models.DTOs
 {
     public class PagedCustomerDTO
     {
+        [JsonPropertyName("results")] // Maps the JSON "results" to the "Customers" property
         public List<Customer> Customers { get; set; } = new List<Customer>();
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+
+        [JsonPropertyName("totalCount")] // Maps JSON "totalCount" to this property
         public int TotalCount { get; set; } = 0;
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
