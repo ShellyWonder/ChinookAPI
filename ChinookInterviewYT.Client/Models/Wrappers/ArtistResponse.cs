edc2a3bab3efc358a988148d0b4a207d4ae8c28b
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-09 19:16:54 -0600

    Implemented Challenge two API on frontend.

---
+++ ChinookInterviewYT.Client/Models/Wrappers/ArtistResponse.cs
@@ -0,0 +1,11 @@
+﻿using ChinookInterviewYT.Client.Models.DTOs;
+
+namespace ChinookInterviewYT.Client.Models.Wrappers
+{
+    public class ArtistsResponse
+    {
+        public object? Result { get; set; } // Expecting this to remain null or unused
+        public PagedArtistDTO? Value { get; set; }  // Contains the paginated artist data
+    }
+
+}
