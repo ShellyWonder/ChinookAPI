b859348cf8367663aeb0a522229525f50beac66c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-11 17:35:12 -0600

    Changed name of SearchResultDTO to align with other classes for reuse. Added CustomerDTO for use in         GetInvoicesWithLineItems().

--- ChinookInterviewYT.Client/Models/DTOs/PagedResultDTO.cs
+++ ChinookInterviewYT.Client/Models/DTOs/PagedResultDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChinookInterviewYT.Client.Models.DTOs
 {
-    public class SearchResultDTO<T>
+    public class PagedResultDTO<T>
     {
         public List<T>? Results { get; set; }
         public int TotalCount { get; set; }
