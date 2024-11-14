6ffc6cdb785b5d9ea6bbf5782a876f8a5bd94c3c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-14 14:52:28 -0600

    Modified Challenge 3 UI to resolve      pagedCustomers generated JSON data display issue .

--- ChinookInterviewYT.Client/Models/Wrappers/InvoiceResponseWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using ChinookInterviewYT.Client.Models.DTOs;
-
-namespace ChinookInterviewYT.Client.Models.Wrappers
-{
-    public class InvoiceResponseWrapper
-    {
-        public PagedCustomerInvoiceDTO? Results { get; set; }
-    }
-}
