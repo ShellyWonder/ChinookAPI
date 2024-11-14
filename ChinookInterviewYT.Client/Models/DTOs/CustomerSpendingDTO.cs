6abd7b4db581cd457159a0ac476db856eeeb4b7e
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-14 17:31:05 -0600

    Challenge Four: Created needed DTO

---
+++ ChinookInterviewYT.Client/Models/DTOs/CustomerSpendingDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ChinookInterviewYT.Client.Models.DTOs
+{
+    public class CustomerSpendingDTO
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public int TotalInvoices { get; set; }
+        public decimal TotalAmountSpent { get; set; }
+        public decimal PercentageOfTotalRevenue { get; set; }
+    }
+}
